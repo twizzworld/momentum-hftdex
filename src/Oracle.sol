9882dd90c67fcb59810bc21c0cea32f3050c8cca
twizzworld <132231765+twizzworld@users.noreply.github.com>
2023-12-26 19:48:20 -0600

    created oracle

---
+++ src/Oracle.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface ITransparentForwarder {
+    function getResult(bytes32 _name) external payable returns (uint256, int8);
+}
+
+contract PriceOracle {
+    ITransparentForwarder public transparentForwarder;
+    uint256 public latestResult;
+    int8 public latestPower;
+
+    constructor(address _forwarderAddress) {
+        transparentForwarder = ITransparentForwarder(_forwarderAddress);
+    }
+
+    function fetchPrice(bytes32 assetName) public payable returns (uint256, int8) {
+        (uint256 result, int8 power) = transparentForwarder.getResult{ value: msg.value }(assetName);
+        latestResult = result;
+        latestPower = power;
+        return (result, power);
+    }
+
+    function calculatePrice() public view returns (uint256) {
+        return latestResult * 10 ** -latestPower;
+    }
+}
